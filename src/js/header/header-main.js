09922d28b572ad43fde62bd225a2e7a7384bffaf
Vitalii <65967741+SolonukhaVitalii@users.noreply.github.com>
2020-12-19 09:46:42 +0200

    copy

---
+++ src/js/header/header-main.js
@@ -0,0 +1,13 @@
+import './logout';
+import './login';
+import './search-model-form';
+import './test-hbs';
+import './register';
+
+// import '../my-cabinet/card-favorites';
+
+import './menuBtn';
+// import './card-favorites';
+
+import './pagination';
+import './searchApiServis';
